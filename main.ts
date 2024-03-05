49accd7f5299ecb183e4a4cf0ab79a8b0f116100
HAHAHAHAHAHAHahahgnjagvb <162365366+HAHAHAHAHAHAHahahgnjagvb@users.noreply.github.com>
2024-03-05 11:59:27 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ namespace gamerFart69{
      * Skibidi
      */
     //% blockId="gamerFart69skibiditoilet"
-    //% block="say skibidi toilet $num"
+    //% block="say $text $num"
     //% num.min=1 num.max=69
-    export function skibiditoilet(num:number){
+    export function skibiditoilet(text:string,num:number){
         if (num <= 0){
             return;
         }
         for (let i = 0; i < num; i++) {
-            basic.showString("skibidi")
+            basic.showString(text)
         }
     }
 }
