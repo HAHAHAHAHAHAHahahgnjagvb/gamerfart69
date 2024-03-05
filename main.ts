d1ae7167487c6659c1d9ddec5c3d901497c9350c
HAHAHAHAHAHAHahahgnjagvb <162365366+HAHAHAHAHAHAHahahgnjagvb@users.noreply.github.com>
2024-03-05 12:13:48 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,13 +8,9 @@ namespace gamerFart7000{
      */
     //% blockId="gamerFart7000skibiditoilet"
     //% block="say Skibidi"
-    //% num.min=1 num.max=69
-    export function skibiditoilet(text:string,num:number){
-        if (num <= 0){
-            return;
-        }
-        for (let i = 0; i < num; i++) {
-            basic.showString(text)
+    //% num.min=1 num.max=70
+    export function skibiditoilet(){
+      basic.showString(skibidi)
         }
     }
     /**
