647d3c11f67bd1c649e866a7bc1dca557d830536
HAHAHAHAHAHAHahahgnjagvb <162365366+HAHAHAHAHAHAHahahgnjagvb@users.noreply.github.com>
2024-03-05 11:59:43 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ namespace gamerFart69{
      * Skibidi
      */
     //% blockId="gamerFart69skibiditoilet"
-    //% block="say $text $num"
+    //% block="say $text $num times"
     //% num.min=1 num.max=69
     export function skibiditoilet(text:string,num:number){
         if (num <= 0){
