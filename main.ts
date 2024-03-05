a7217a603afa714136b9ea18cd82c3bef7cdda13
HAHAHAHAHAHAHahahgnjagvb <162365366+HAHAHAHAHAHAHahahgnjagvb@users.noreply.github.com>
2024-03-05 11:52:42 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+namespace gamerFart69{
+    /**
+     * Skibidi
+     */
+    //% blockId="gamerFart69skibiditoilet"
+    //% block="say skibidi toilet"
+    export function skibiditoilet(){
+        basic.showString("skibidi toilet")
+    }
+}
